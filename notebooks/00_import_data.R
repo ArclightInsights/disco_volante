6d299b9eb83d1eaba57f23eaca019f3beadc77bd
MattLScruggs <matthewls01@outlook.com>
2026-05-27 23:51:02 -0500

    updated code

--- notebooks/00_import_data.R
+++ notebooks/00_import_data.R
@@ -1,3 +1,14 @@
 library(jsonlite)
 library(httr2)
 library(tidyverse)
+
+NSSP_Combined_ED_Visits_ENDPOINT="https://data.cdc.gov/api/v3/views/7xva-uux8/query.json"
+API_token<-Sys.getenv("Socrata-token")
+
+header_list<-list("Accept" == "application/json")
+
+req<-request(NSSP_Combined_ED_Visits_ENDPOINT)|>
+  req_headers(Accept = "application/json", limit = "100000", X-App-Token = API_token)
+  
+
+  
